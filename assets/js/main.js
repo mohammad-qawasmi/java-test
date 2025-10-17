dd515b2391310d3da637b86635eba63dfeffb48a
mohammad-qawasmi <steps.sys.steps@gmail.com>
2025-10-17 18:46:10 +0300

    java-task

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+
+
+
+document.querySelector(".add-edu").onclick = function() {
+    
+    document.querySelector("p").textContent = "software emgineer";
+    
+}
+
+
+document.querySelector(".add-dark").onclick = function() {
+
+document.querySelector("body").classList.add("dark-mode");
+}
+
+document.querySelector(".add-light").onclick = function() {
+
+document.querySelector("body").classList.remove("dark-mode");
+}
+
+document.querySelector(".Change").onclick = function(){
+document.querySelector("body").classList.toggle("dark-mode")
+}
